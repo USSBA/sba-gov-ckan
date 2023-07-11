d58cf03cca638603c2ba8549f7ffb1a1c83a40f4
Tyler <tg_webb@yahoo.com>
2023-07-11 12:35:03 -0700

    IA-3181: Adjusted s3 backend for tf validate job

--- terraform/applications/versions.tf
+++ terraform/applications/versions.tf
@@ -11,12 +11,3 @@ provider "aws" {
   region              = "us-east-1"
   allowed_account_ids = ["230968663929"]
 }
-terraform {
-  backend "s3" {
-    bucket         = "sbagovlower-terraform-remote-state"
-    key            = "ckan.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locktable"
-    acl            = "bucket-owner-full-control"
-  }
-}
