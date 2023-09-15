9660fafba800bb0569d33dd9ae2451c08a548804
mshiyk <mshiyk@fearless.tech>
2023-09-15 16:56:55 -0500

    IA-3315 Updated backend, and database configuration in the application state.

--- terraform/applications/s3-backend.tf
+++ terraform/applications/s3-backend.tf
@@ -1,9 +1,19 @@
 terraform {
   backend "s3" {
-    bucket         = "sbagovlower-terraform-remote-state"
+    bucket         = "sba-ckan-terraform-remote-state"
     key            = "ckan.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-locktable"
+    dynamodb_table = "sba-ckan-terraform-state-locktable"
     acl            = "bucket-owner-full-control"
   }
 }
+
+#terraform {
+#  backend "s3" {
+#    bucket         = "sbagovlower-terraform-remote-state"
+#    key            = "ckan.tfstate"
+#    region         = "us-east-1"
+#    dynamodb_table = "terraform-state-locktable"
+#    acl            = "bucket-owner-full-control"
+#  }
+#}
---
+++ terraform/databases/aurora-serverless.tf
@@ -0,0 +1,62 @@
+resource "random_password" "ckan_rds_pass" {
+  length  = 16
+  special = false
+  keepers = {
+    # Update this to trigger a password change
+    trigger_new_pass = "1"
+  }
+}
+
+resource "aws_ssm_parameter" "ckan_rds_pass" {
+  name  = "/${terraform.workspace}/ckan/rds/pass"
+  type  = "SecureString"
+  value = random_password.ckan_rds_pass.result
+}
+
+
+module "ckan_db" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "~> 8.3"
+
+  name = "${terraform.workspace}-ckan"
+
+  master_username             = "ckan_default"
+  master_password             = random_password.ckan_rds_pass.result
+  manage_master_user_password = false
+  deletion_protection         = true
+
+  engine               = "aurora-postgresql"
+  engine_version       = "11.19"
+  engine_mode          = "serverless"
+  enable_http_endpoint = true
+  network_type         = "IPV4"
+
+  subnets                         = data.aws_subnets.private.ids
+  create_db_subnet_group          = true
+  vpc_id                          = data.aws_vpc.ckan.id
+  monitoring_interval             = 60
+  apply_immediately               = true #local.env.apply_db_changes_immediately
+  storage_encrypted               = true
+  #db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.ckan_rds.id
+
+  skip_final_snapshot       = true #local.env.db_skip_final_snapshot
+  final_snapshot_identifier = "final-${terraform.workspace}-ckan"
+
+  database_name = "ckan_default"
+
+  scaling_configuration = {
+    auto_pause               = false # in dev we should probably set this to true
+    seconds_until_auto_pause = 300   # with a auto_pause of about 30-60 minutes or so
+    max_capacity             = 64
+    min_capacity             = contains(["prod"], terraform.workspace) ? 32 : 4
+    timeout_action           = "RollbackCapacityChange"
+  }
+
+  copy_tags_to_snapshot = true
+
+  tags = {
+    BackupDaily     = terraform.workspace == "prod" ? true : false
+    BackupQuarterly = terraform.workspace == "prod" ? true : false
+    BackupWeekly    = terraform.workspace == "prod" ? true : false
+  }
+}
