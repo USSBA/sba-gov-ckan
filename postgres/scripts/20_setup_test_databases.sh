d8e1e1effed1e20d0bf8cb455e75316077e42574
Tyler <tg_webb@yahoo.com>
2024-03-04 13:53:34 -0800

    IA-3524: Found and fixed issue with Xloader not parsing private files; Issue is due to values not existing in ckan.ini

--- postgres/scripts/20_setup_test_databases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "ckan_default" <<-EOSQL
-    CREATE DATABASE ckan_test OWNER "ckan_default" ENCODING 'utf-8';
-    CREATE DATABASE datastore_test OWNER "ckan_default" ENCODING 'utf-8';
-EOSQL
